7177073f8d04461c2ea98e790337e674b1e216b5
John Martinez <jmartinez729@gmail.com>
2018-11-09 16:56:12 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,30 @@
+# def get_first_name_of_season_winner(data, season)
+#   data[season].each do |season, season_array|
+#     season_array.each do |contestant_information|
+#       if contestant_information[:status] == "Winner"
+#         return contestant_information[:name].split(" ")[0]
+#       end
+#     end
+#   end
+# end
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants|
+    if contestants["status"] == "Winner"
+      return contestants["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-end
+  data.each do |season, season_array|
+    season_array.each do |contestant_information|
+      if contestant_information["occupation"] == occupation
+        return contestant_information["name"]
+      end
+    end 
+  end 
+end 
 
 def count_contestants_by_hometown(data, hometown)
   # code here
